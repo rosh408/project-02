87dfa6034005df4803cb9908fba782d61a3d92e5
Rosh <rosh_408@hotmail.com>
2019-04-25 13:12:58 -0700

    create svg/add svg settings/append svg to html

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,26 @@
+import { SVG_NS } from "../settings";
+
 export default class Game {
+
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+
+    this.gameElement = document.getElementById(this.element);
+    // Other code goes here...
   }
 
   render() {
-		// More code goes here....
+    // More code goes here....
+    //add the code from the slide
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+
+    this.gameElement.innerHTML = '';
   }
 }
